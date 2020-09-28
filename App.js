da6e913bdf512d26c66fc05d72b9cb26abc1286a
Chris Leja <christopher.leja@gmail.com>
2020-09-27 21:29:56 -0700

    start adding navigators

--- App.js
+++ App.js
@@ -38,8 +38,38 @@ import * as Permissions from 'expo-permissions'
 import ImageInput from './src/components/ImageInput';
 import ImageInputList from './src/components/ImageInputList';
 
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native'
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Tweet</Text>
+    <Button
+      title="View Tweet"
+      onPress={() => navigation.navigate("TweetDetails")}
+    />
+  </Screen>
+)
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweetails</Text>
+  </Screen>
+)
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets}/>
+    <Stack.Screen name="TweetDetails" component={TweetDetails}/>
+  </Stack.Navigator>
+)
+
 export default function App() {
-  
-  return <ListingEditScreen />
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  )
 }
 
